dc0fb25c88cc2cd155ffe6351099e2da2d41e630
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-01 21:16:29 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -4,3 +4,6 @@ server_ip = "127.0.0.1"
 server_port = 12345
 # Crear un socket del servidor
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Enlazar el socket a la dirección IP y puerto
+server_socket.bind((server_ip, server_port))
