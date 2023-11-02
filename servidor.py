7a213903c53e05f066b2c7dd55e7d817ea65a326
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-01 21:16:16 -0500

    Update servidor.py

--- servidor.py
+++ servidor.py
@@ -2,3 +2,5 @@ import socket
 # Configuración del servidor
 server_ip = "127.0.0.1"
 server_port = 12345
+# Crear un socket del servidor
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
