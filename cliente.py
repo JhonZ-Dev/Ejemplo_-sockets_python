9c6b7082e8a4f0347319fb6c5be31cc3569cb3c2
Jhon Zambrano <79710173+jhonZambrano1999@users.noreply.github.com>
2023-11-03 20:48:56 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,22 +1,30 @@
 import socket
-# Configuración del cliente
-server_ip = "127.0.0.1"
-server_port = 12345
 
-# Crear un socket
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def run_client():
+    server_ip = "127.0.0.1"
+    server_port = 12345
 
-# Conectar al servidor
-client_socket.connect((server_ip, server_port))
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-while True:
-     # Enviar un mensaje al servidor
-    message = input("Tú: ")
-    client_socket.send(message.encode())
-    # Recibir mensaje del servidor
-    response = client_socket.recv(1024).decode()
-    print(f"Servidor: {response}")
-    if message.lower() == "adiós":
-        print("Conexión terminada.")
+    try:
+        client_socket.connect((server_ip, server_port))
+        while True:
+            message = input("Tú: ")
+            client_socket.send(message.encode())
+
+            if message.lower() == "adiós":
+                print("Conexión terminada.")
+                client_socket.close()
+                break
+
+            response = client_socket.recv(1024).decode()
+            print(f"Servidor: {response}")
+
+    except ConnectionRefusedError:
+        print("No se pudo establecer conexión. Asegúrate de que el servidor esté en ejecución.")
+    except Exception as e:
+        print(f"Error: {e}")
         client_socket.close()
-        break
+
+if __name__ == "__main__":
+    run_client()
